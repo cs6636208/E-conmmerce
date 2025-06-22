22fec56dd690f9a9455ff9e7b1142d2615301287
Your Name <you@example.com>
2025-06-22 15:20:12 +0700

    second commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,12 @@
 // Functions
+const prisma = require("../config/prisma");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     // Step 1: Validate Body
     if (!email) {
       return res.status(400).json({
@@ -14,9 +18,33 @@ exports.register = async (req, res) => {
         message: "Password is required!!!",
       });
     }
+
     // Step 2: Check Email in DB already exists ?
-    console.log(email, password);
-    res.send("Hello Register In Controllers");
+    const user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+      return res.status(400).json({
+        message: "Email already exists!!!",
+      });
+    }
+
+    // Step 3: Hash Password
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // Step 4: Create Register
+
+    await prisma.user.create({
+      data: {
+        email: email,
+        password: hashPassword,
+      },
+    });
+
+    res.send("Registered Successfully");
   } catch (err) {
     console.log(err);
     res.status(500).json({
@@ -27,8 +55,57 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    res.send("Hello Login In Controllers");
+    // code
+    const { email, password } = req.body;
+
+    // Step 1: Check Email
+    const user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (!user || !user.enabled) {
+      return res.status(400).json({
+        message: "User Not found or not Enabled",
+      });
+    }
+
+    // Step 2: Check Password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Password Invalid",
+      });
+    }
+
+    // Step 3: Create Payload
+    const payload = {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    };
+
+    // Step 4: Generate JWT Token
+    jwt.sign(
+      payload,
+      process.env.SECRET,
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) {
+          return res.status(500).json({
+            message: "Server Error",
+          });
+        }
+        res.json({
+          payload,
+          token,
+        });
+      }
+    );
   } catch (err) {
+    // err
     console.log(err);
     res.status(500).json({
       message: "Server Error",
--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,9 +1,17 @@
 // Functions
 
+const prisma = require("../config/prisma");
+
 exports.create = async (req, res) => {
   try {
     // code
-    res.send("Hello Category");
+    const { name } = req.body;
+    const category = await prisma.category.create({
+      data: {
+        name: name,
+      },
+    });
+    res.send(category);
   } catch (err) {
     console.log(err);
     res.status(500).json({
@@ -15,7 +23,8 @@ exports.create = async (req, res) => {
 exports.list = async (req, res) => {
   try {
     // code
-    res.send("Hello Category List");
+    const category = await prisma.category.findMany();
+    res.send(category);
   } catch (err) {
     console.log(err);
     res.status(500).json({
@@ -28,8 +37,12 @@ exports.remove = async (req, res) => {
   try {
     // code
     const { id } = req.params;
-    console.log(id);
-    res.send("Hello Category Remove");
+    const category = await prisma.category.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    res.send(category);
   } catch (err) {
     console.log(err);
     res.status(500).json({
