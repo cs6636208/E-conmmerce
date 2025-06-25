3987982928471ac43cf7c2e40bc4cef921c212f0
Your Name <you@example.com>
2025-06-25 21:57:13 +0700

    user routes

---
+++ server/routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { authCheck } = require("../middlewares/authCheck");
+const { listUsers } = require("../controllers/user");
+
+router.get("/users", authCheck, listUsers);
+// router.post("/change-status");
+// router.post("/change-role");
+// router.post("/user/cart");
+// router.get("/user/cart");
+// router.delete("/user/cart");
+// router.post("/user/address");
+// router.post("/user/order");
+// router.get("/user/order");
+
+module.exports = router;
