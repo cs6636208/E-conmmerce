4c26ed9fc4f0fcafb5956665530067b11b67990d
Your Name <you@example.com>
2025-06-26 22:25:06 +0700

    update user controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,9 +1,126 @@
 // Functions
+const prisma = require("../config/prisma");
 
 exports.listUsers = async (req, res) => {
   try {
     // code
-    res.send("Hello Users");
+    const user = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        enabled: true,
+        address: true,
+      },
+    });
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.changeStatus = async (req, res) => {
+  try {
+    // code
+    const { id, enabled } = req.body;
+    console.log(id, enabled);
+
+    const user = await prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        enabled: enabled,
+      },
+    });
+    res.send("Update Status Success");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.changeRole = async (req, res) => {
+  try {
+    // code
+    res.send("Hello changeRole");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.userCart = async (req, res) => {
+  try {
+    // code
+    res.send("Hello userCart");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.getUserCart = async (req, res) => {
+  try {
+    // code
+    res.send("Hello getUserCart");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.emptyCart = async (req, res) => {
+  try {
+    // code
+    res.send("Hello emptyCart");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.saveAddress = async (req, res) => {
+  try {
+    // code
+    res.send("Hello saveAddress");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.saveOrder = async (req, res) => {
+  try {
+    // code
+    res.send("Hello saveOrder");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.getOrder = async (req, res) => {
+  try {
+    // code
+    res.send("Hello getOrder");
   } catch (err) {
     console.log(err);
     res.status(500).json({
