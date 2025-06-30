a2d60757452b6ddf959cd84bf10cd65af41966f5
Your Name <you@example.com>
2025-06-30 23:32:07 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// rafce
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+
+const App = () => {
+  // Javascript
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
+};
+
+export default App;
