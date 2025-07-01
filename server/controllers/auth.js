e1a122b6f938e854a0b9b5acbb7cec9d53a3760d
Your Name <you@example.com>
2025-07-01 23:27:07 +0700

    updated auth controllers

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -115,7 +115,18 @@ exports.login = async (req, res) => {
 
 exports.currentUser = async (req, res) => {
   try {
-    res.send("Hello current user");
+    const user = await prisma.user.findFirst({
+      where: {
+        email: req.user.email,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+      },
+    });
+    res.json({ user });
   } catch (err) {
     console.log(err);
     res.status(500).json({
