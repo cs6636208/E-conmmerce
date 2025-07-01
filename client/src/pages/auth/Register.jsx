73bf26a7886cb68d5e1fc280d1fccbe473c0e31f
Your Name <you@example.com>
2025-07-01 14:33:36 +0700

    update Register

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Register = () => {
-  return <div>Register</div>;
+  // javascript
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleOnChange = (e) => {
+    // code
+    console.log(e.target.name, e.target.value);
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (form.password !== form.confirmPassword) {
+      return alert("Confirm Password is not match!");
+    }
+    console.log(form);
+    try {
+      const res = await axios.post("http://localhost:5000/api/register", form);
+      console.log(res.data);
+      toast.success(res.data);
+    } catch (err) {
+      const errMsg = err.response?.data?.message;
+      toast.error(errMsg);
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      Register
+      <form onSubmit={handleSubmit}>
+        Email
+        <input
+          className="border"
+          onChange={handleOnChange}
+          name="email"
+          type="email"
+        />
+        Password
+        <input
+          className="border"
+          onChange={handleOnChange}
+          name="password"
+          type="password"
+        />
+        Confirm Password
+        <input
+          className="border"
+          onChange={handleOnChange}
+          name="confirmPassword"
+          type="password"
+        />
+        <button className="bg-blue-500 rounded-md">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
