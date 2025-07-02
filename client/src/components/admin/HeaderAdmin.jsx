ddf6bdb17f4da8234aba72a269d3b5db08b1c807
Your Name <you@example.com>
2025-07-02 14:05:46 +0700

    add HeaderAdmin

---
+++ client/src/components/admin/HeaderAdmin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderAdmin = () => {
+  return (
+    <header className="bg-slate-500 h-16 flex items-center px-6 text-slate-200">
+      Header
+    </header>
+  );
+};
+
+export default HeaderAdmin;
