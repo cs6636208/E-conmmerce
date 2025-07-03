6c3fac9cfff0445947ac1932070d02044152e7f3
Your Name <you@example.com>
2025-07-03 22:40:47 +0700

    update Category

--- client/src/pages/admin/Category.jsx
+++ client/src/pages/admin/Category.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import FormCategory from "../../components/admin/FormCategory";
 
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <div>
+      <FormCategory />
+    </div>
+  );
 };
 
 export default Category;
