3ff4637ea6da685c2b338d60c4ea9bb25985a75b
Your Name <you@example.com>
2025-07-05 23:06:13 +0700

    update ecom-store

--- client/src/store/ecom-store.jsx
+++ client/src/store/ecom-store.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
+import { listCategory } from "../api/Category";
+import { listProduct } from "../api/product";
 
 const ecomStore = (set) => ({
   user: null,
   token: null,
+  categories: [],
+  products: [],
   actionLogin: async (form) => {
     const res = await axios.post("http://localhost:5000/api/login", form);
     set({
@@ -12,6 +16,22 @@ const ecomStore = (set) => ({
       token: res.data.token,
     });
     return res;
+  },
+  getCategory: async (token) => {
+    try {
+      const res = await listCategory(token);
+      set({ categories: res.data });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getProduct: async (token, count) => {
+    try {
+      const res = await listProduct(token, count);
+      set({ products: res.data });
+    } catch (err) {
+      console.log(err);
+    }
   },
 });
 
