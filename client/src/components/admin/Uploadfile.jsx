e34ed7356d49eaebce590269e69b6eb7648b5e56
Your Name <you@example.com>
2025-07-26 15:37:49 +0700

    updated Uploadfile admin client

--- client/src/components/admin/Uploadfile.jsx
+++ client/src/components/admin/Uploadfile.jsx
@@ -3,12 +3,15 @@ import { toast } from "react-toastify";
 import Resize from "react-image-file-resizer";
 import { removeFiles, uploadFiles } from "../../api/product";
 import useEcomStore from "../../store/ecom-store";
+import { Loader } from "lucide-react";
 
 const Uploadfile = ({ form, setForm }) => {
   const token = useEcomStore((state) => state.token);
   const [isLoading, setIsLoading] = useState(false);
+
   const handleOnChange = (e) => {
     // code
+    setIsLoading(true);
     const files = e.target.files;
     if (files) {
       setIsLoading(true);
@@ -40,10 +43,12 @@ const Uploadfile = ({ form, setForm }) => {
                   ...form,
                   images: allFiles,
                 });
+                setIsLoading(false);
                 toast.success("Upload image Success!!!");
               })
               .catch((err) => {
                 console.log(err);
+                setIsLoading(false);
               });
           },
           "base64"
@@ -76,6 +81,8 @@ const Uploadfile = ({ form, setForm }) => {
   return (
     <div className="my-4">
       <div className="flex mx-4 gap-4 my-4">
+        {isLoading && <Loader className="w-16 h-16 animate-spin" />}
+
         {/* image */}
         {form.images.map((item, index) => (
           <div className="relative" key={index}>
