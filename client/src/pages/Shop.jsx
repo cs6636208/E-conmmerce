df5dadbcac71179a3ce36ab2f98e5da1080feadc
Your Name <you@example.com>
2025-08-26 13:27:12 +0700

    update client Shop

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ProductCard from "../components/card/ProductCard";
+import useEcomStore from "../store/ecom-store";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  const getProduct = useEcomStore((state) => state.getProduct);
+  const products = useEcomStore((state) => state.products);
+  useEffect(() => {
+    getProduct();
+  }, []);
+  return (
+    <div className="flex">
+      {/* Search Bar */}
+      <div className="w-1/4 p-4 bg-slate-100 h-screen">Searchbar</div>
+      {/* Product */}
+      <div className="w-1/2 p-4 h-screen overflow-y-auto">
+        <p className="text-2xl font-bold mb-4">สินค้าทั้งหมด</p>
+        <div className="flex flex-wrap gap-4">
+          {/* Product Card */}
+          {products.map((item, index) => (
+            <ProductCard item={item} key={index} />
+          ))}
+
+          {/* Product Card */}
+        </div>
+      </div>
+      {/* Cart */}
+      <div className="w-1/4 p-4 bg-slate-100 h-screen overflow-y-auto">
+        Cart
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
