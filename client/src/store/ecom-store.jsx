90f77868509cc422a3681efbdb956132814b2a92
Your Name <you@example.com>
2025-08-26 21:53:38 +0700

    update austand client ecom-store1

--- client/src/store/ecom-store.jsx
+++ client/src/store/ecom-store.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 import { listCategory } from "../api/Category";
-import { listProduct } from "../api/product";
+import { listProduct, searchFilters } from "../api/product";
 
 const ecomStore = (set) => ({
   user: null,
@@ -28,6 +28,14 @@ const ecomStore = (set) => ({
   getProduct: async (count) => {
     try {
       const res = await listProduct(count);
+      set({ products: res.data });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  actionSearchFilters: async (arg) => {
+    try {
+      const res = await searchFilters(arg);
       set({ products: res.data });
     } catch (err) {
       console.log(err);
