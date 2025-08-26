7603531f8cf7240983a552c1dc94ddb4b43e0919
Your Name <you@example.com>
2025-08-26 13:25:22 +0700

    update client Category

--- client/src/api/Category.jsx
+++ client/src/api/Category.jsx
@@ -9,13 +9,9 @@ export const createCategory = async (token, form) => {
   });
 };
 
-export const listCategory = async (token) => {
+export const listCategory = async () => {
   // code body
-  return axios.get("http://localhost:5000/api/category", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  return axios.get("http://localhost:5000/api/category");
 };
 
 export const removeCategory = async (token, id) => {
